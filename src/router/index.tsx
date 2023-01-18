8eb9601e2e9d08f5f7e621401533cd9d6c404827
michalskimateusz <michalskimateusz@outlook.com>
2023-01-18 21:47:11 +0100

    Router refactor

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,32 +1,28 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route
-} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import RootLayout from '../layouts/RootLayout/RootLayout'
 import HomeView from '../views/HomeView/HomeView'
 import GalleryView from '../views/GalleryView/GalleryView'
 import NotFound from '../views/NotFound/NotFound'
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      element={<RootLayout />}
-    >
-      <Route
-        index
-        element={<HomeView />}
-      />
-      <Route
-        path="/gallery"
-        element={<GalleryView />}
-        errorElement={<NotFound />}
-      />
-      <Route
-        path="*"
-        element={<NotFound />}
-      />
-    </Route>
-  )
-)
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomeView />,
+        index: true
+      },
+      {
+        path: '/gallery',
+        element: <GalleryView />
+      },
+      {
+        path: '/*',
+        element: <NotFound />
+      }
+    ]
+  }
+])
